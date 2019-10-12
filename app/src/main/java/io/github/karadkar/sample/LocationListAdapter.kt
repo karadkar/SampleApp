02233adf543a5d3f913c8a8db45f856703a2427e
Rohit Karadkar <karadkar.rohit@gmail.com>
2019-10-12 16:32:41 +0530

    added list adapter

---
+++ app/src/main/java/io/github/karadkar/sample/LocationListAdapter.kt
@@ -0,0 +1,76 @@
+package io.github.karadkar.sample
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import io.github.karadkar.sample.databinding.LocationListItemBinding
+
+class LocationListAdapter(val context: Context, val onClickItem: (item: LocationListItem) -> Unit) :
+    ListAdapter<LocationListItem, LocationListAdapter.VH>(LocationListDiff()) {
+    private val icFavourite by lazy {
+        ContextCompat.getDrawable(context, R.drawable.ic_favorite)
+    }
+    private val icFavouriteBorder by lazy {
+        ContextCompat.getDrawable(context, R.drawable.ic_favorite_border)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val binding = DataBindingUtil.inflate<LocationListItemBinding>(
+            LayoutInflater.from(context),
+            R.layout.location_list_item,
+            parent,
+            false
+        )
+        return VH(binding)
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        if (position in 0 until itemCount) {
+            holder.bind(getItem(position))
+        }
+    }
+
+    inner class VH(private val binding: LocationListItemBinding) : RecyclerView.ViewHolder(binding.root),
+        View.OnClickListener {
+        init {
+            binding.root.setOnClickListener(this)
+        }
+
+        fun bind(item: LocationListItem) {
+            binding.apply {
+                tvLocationName.text = item.title
+                btnLocationDate.text = item.date
+                btnFavourite.background = (if (item.isFavourite) icFavourite else icFavouriteBorder)
+            }
+            if (item.image.isNotEmpty()) {
+                Picasso.get()
+                    .load(item.image)
+                    .into(binding.ivLocationImage)
+            }
+            binding.executePendingBindings()
+        }
+
+        override fun onClick(view: View?) {
+            if (adapterPosition in 0 until itemCount) {
+                onClickItem(getItem(adapterPosition))
+            }
+        }
+    }
+
+    class LocationListDiff() : DiffUtil.ItemCallback<LocationListItem>() {
+        override fun areItemsTheSame(oldItem: LocationListItem, newItem: LocationListItem): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: LocationListItem, newItem: LocationListItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/io/github/karadkar/sample/MainActivity.kt
+++ app/src/main/java/io/github/karadkar/sample/MainActivity.kt
@@ -1,12 +1,36 @@
 package io.github.karadkar.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import io.github.karadkar.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val adapter = LocationListAdapter(this) { clickedListItem ->
+            Toast.makeText(this, "clicked ${clickedListItem.title}", Toast.LENGTH_SHORT).show()
+        }
+        binding.rvLocations.adapter = adapter
+        adapter.submitList(dummyListItems())
+    }
+
+    fun dummyListItems(): MutableList<LocationListItem> {
+        val list = mutableListOf<LocationListItem>()
+        for (i in 0..10) {
+            list.add(
+                LocationListItem(
+                    title = "Location name $i",
+                    date = "June 19 - 20, 2019",
+                    isFavourite = i % 2 == 0,
+                    image = "https://picsum.photos/300"
+                )
+            )
+        }
+        return list
     }
 }
