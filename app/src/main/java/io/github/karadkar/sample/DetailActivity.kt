78bf10efba8a5338d285ca87dd9869e5495e5844
Rohit Karadkar <karadkar.rohit@gmail.com>
2019-10-13 13:20:51 +0530

    detail screen

---
+++ app/src/main/java/io/github/karadkar/sample/DetailActivity.kt
@@ -0,0 +1,45 @@
+package io.github.karadkar.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.squareup.picasso.Picasso
+import io.github.karadkar.sample.databinding.DetailActivityBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    lateinit var binding: DetailActivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.detail_activity)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        Picasso.get().load("https://picsum.photos/500").into(binding.ivLocationImage)
+        binding.tvDate.text = "June 19 - 29, 2019"
+        binding.tvDescription.text =
+            "A common collapsing toolbar pattern is the parallax image scroll, in which images or other children of the CollapsingToolbarLayout scroll at different rates than their siblings. To achieve this effect, add an ImageView and any other views as children of the CollapsingToolbarLayout. Specify parallax multipliers in the XML for as many of the siblings as you like." +
+                    "A common collapsing toolbar pattern is the parallax image scroll, in which images or other children of the CollapsingToolbarLayout scroll at different rates than their siblings. To achieve this effect, add an ImageView and any other views as children of the CollapsingToolbarLayout. Specify parallax multipliers in the XML for as many of the siblings as you like." +
+                    "A common collapsing toolbar pattern is the parallax image scroll, in which images or other children of the CollapsingToolbarLayout scroll at different rates than their siblings. To achieve this effect, add an ImageView and any other views as children of the CollapsingToolbarLayout. Specify parallax multipliers in the XML for as many of the siblings as you like." +
+                    "A common collapsing toolbar pattern is the parallax image scroll, in which images or other children of the CollapsingToolbarLayout scroll at different rates than their siblings. To achieve this effect, add an ImageView and any other views as children of the CollapsingToolbarLayout. Specify parallax multipliers in the XML for as many of the siblings as you like." +
+                    "A common collapsing toolbar pattern is the parallax image scroll, in which images or other children of the CollapsingToolbarLayout scroll at different rates than their siblings. To achieve this effect, add an ImageView and any other views as children of the CollapsingToolbarLayout. Specify parallax multipliers in the XML for as many of the siblings as you like."
+
+    }
+
+    fun dummyListItems(): MutableList<LocationListItem> {
+        val list = mutableListOf<LocationListItem>()
+        for (i in 0..10) {
+            list.add(
+                LocationListItem(
+                    title = "Location name $i",
+                    date = "June 19 - 20, 2019",
+                    isFavourite = i % 2 == 0,
+                    image = "https://picsum.photos/300"
+                )
+            )
+        }
+        return list
+    }
+}
--- app/src/main/java/io/github/karadkar/sample/MainActivity.kt
+++ app/src/main/java/io/github/karadkar/sample/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.github.karadkar.sample
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import io.github.karadkar.sample.databinding.ActivityMainBinding
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val adapter = LocationListAdapter(this) { clickedListItem ->
-            Toast.makeText(this, "clicked ${clickedListItem.title}", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, DetailActivity::class.java))
         }
         binding.rvLocations.adapter = adapter
         adapter.submitList(dummyListItems())
