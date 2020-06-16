0e6047984e76487b746912112be496df36845de9
Rohit Karadkar <karadkar.rohit@gmail.com>
2020-06-16 17:40:37 +0530

    surface-view: filling the circle

--- app/src/main/java/io/github/karadkar/sample/SurfaceView.kt
+++ app/src/main/java/io/github/karadkar/sample/SurfaceView.kt
@@ -4,8 +4,8 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 
@@ -15,19 +15,26 @@ class SurfaceView : View {
     constructor(context: Context, attrs: AttributeSet, defStyleAttrs: Int) : super(context, attrs, defStyleAttrs)
 
     private val defaultPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val borderPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     init {
         defaultPaint.apply {
             color = ContextCompat.getColor(context, R.color.colorAccent)
+            style = Paint.Style.FILL
+        }
+        borderPaint.apply {
+            color = ContextCompat.getColor(context, R.color.colorAccent)
+            style = Paint.Style.STROKE
+            strokeWidth = 4.toPx.toFloat()
         }
     }
 
     private var progress = 0f
     fun transformToCircle() {
-        val animator = ValueAnimator.ofFloat(0f, 125f)
-        animator.duration = 2000
+        val animator = ValueAnimator.ofFloat(0f, 100f)
+        animator.duration = 300
         animator.addUpdateListener {
-            progress = it.animatedValue as Float
+            progress = (it.animatedValue as Float)
             invalidate()
         }
         animator.start()
@@ -36,6 +43,33 @@ class SurfaceView : View {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         if (canvas == null) return
+
+        progressiveCircle(canvas)
+    }
+
+    private fun progressiveCircle(canvas: Canvas) {
+
+        val midX = width / 2f // middle point on X-axis
+        val midY = height / 2f // middle point on  Y-axis
+        val maxRadius = (maxOf(width, height) / 2f)
+        val progress = (this.progress / 100) // 15% == 0.15
+
+//        Log.e(this.javaClass.simpleName, "progress: $progress")
+        val radius = maxRadius * progress
+
+        canvas.drawCircle(midX, midY, radius, defaultPaint)
+
+        val rect = Rect(0, 0, width, height)
+        // draw rectangle border
+        canvas.drawRect(rect, borderPaint)
+
+        // draw filled rectangle
+        if (progress >= 1.0f) {
+            canvas.drawRect(rect, defaultPaint)
+        }
+    }
+
+    private fun progressingRectandle(canvas: Canvas) {
 
         val midX = width / 2f // middle point on X-axis
         val midY = height / 2f // middle point on  Y-axis
@@ -53,20 +87,9 @@ class SurfaceView : View {
         val rightBotY = midY + radius
 
         canvas.drawRoundRect(leftTopX, leftTopY, rightBotX, rightBotY, radius, radius, defaultPaint)
-        Log.e(
-            "SurfaceView",
-            "radius:$radius leftTopX:$leftTopX, leftTopY$leftTopY, rightBotX$rightBotX, rightBotY:$rightBotY"
-        )
-    }
-
-    private fun drawCircularReveal(canvas: Canvas) {
-
-        val midX = width / 2f // middle point on X-axis
-        val midY = height / 2f // middle point on  Y-axis
-        val maxRadius = (maxOf(width, height) / 2f)
-
-        // 50 * 0.1 = 10
-        val radius = maxRadius * (progress / 100)
-        canvas.drawCircle(midX, midY, radius, defaultPaint)
+//        Log.e(
+//            "SurfaceView",
+//            "radius:$radius leftTopX:$leftTopX, leftTopY$leftTopY, rightBotX$rightBotX, rightBotY:$rightBotY"
+//        )
     }
 }
