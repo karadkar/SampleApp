b826e9066eb057cff7fa60dfe2d3e9c5d7b48501
Rohit Karadkar <karadkar.rohit@gmail.com>
2020-06-22 11:31:47 +0530

    fixed box grid

---
+++ app/src/main/java/io/github/karadkar/sample/BFSPathfinder.kt
@@ -0,0 +1,43 @@
+package io.github.karadkar.sample
+
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.FlowableEmitter
+import java.util.*
+
+class BFSPathfinder() {
+
+
+    fun find(start: GridBox, goal: GridBox, grid: Array<Array<GridBox>>): Flowable<GridBox> {
+        return Flowable.create({ emitter: FlowableEmitter<GridBox> ->
+            val queue: Queue<GridBox> = LinkedList<GridBox>()
+
+            queue.add(start)
+            start.visited = true
+
+            while (queue.isNotEmpty()) {
+                val node = queue.poll()!!
+                val (row: Int, col: Int) = (node.row to node.col)
+
+                // emmit as visited
+                emitter.onNext(node)
+
+                if (node == goal) {
+                    emitter.onComplete()
+                }
+
+                val nexItems = listOf((row + 1 to col), (row to col + 1), (row - 1 to col), (row to col - 1))
+                for ((r, c) in nexItems) {
+                    if (r >= 0 && c >= 0 && r < grid.size && c < grid.first().size && !grid[r][c].visited) {
+                        grid[r][c].visited = true
+                        queue.offer(grid[r][c])
+                    }
+                }
+                Thread.sleep(500L)
+            }
+
+            // finished processing queue
+
+        }, BackpressureStrategy.BUFFER)
+    }
+}
